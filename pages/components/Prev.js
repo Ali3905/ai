0d40e3e62407f45f6d71c9f1b1afa5e294904c93
Ali3905 <muhammadali30905@gmail.com>
2023-10-14 19:45:53 +0500

    UI - fixes

--- pages/components/Prev.js
+++ pages/components/Prev.js
@@ -1,7 +1,7 @@
 import React from "react"
 const Prev = ({onClick}) => {
     return (
-        <svg onClick={onClick} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 absolute top-1/2 left-12 text-white">
+        <svg onClick={onClick} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 absolute top-1/2 left-1 text-white">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
     )
--- pages/components/TestimonialCarousel.js
+++ pages/components/TestimonialCarousel.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import TestimonialCarousel from './components/TestimonialCarousel'
+import Next from './Next';
+import Prev from './Prev';
+import Slider from "react-slick";
 
-const testimonials = () => {
+const TestimonialCarousel = () => {
 
     const testimonials = [{
         testimonial: "Oh my God, this is lifesaver! This saved my few hours. ",
@@ -22,13 +24,49 @@ const testimonials = () => {
     },
     ]
 
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      initialSlide: 0,
+      nextArrow: <Next />,
+      prevArrow: <Prev />,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            initialSlide: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    };
+
 
     return (
         <>
-            <div className='testimonials_page'>
-
-                {testimonials.length > 0 && testimonials.map((ele, i) => {
-                    return <div className="testimonial_card" key={i}
+            <Slider className='testimonial_carousel' {...settings}>
+            {testimonials.length > 0 && testimonials.map((ele, i) => {
+                    return <div className="testimonial_carousel_card" key={i}
                     >
                         <svg className="quotation_mark" width="36" height="27" viewBox="0 0 36 27" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.8904 8.46818V0H14.5479C5.54795 0 0 5.64545 0 16.0773V27H9.86301V16.4455C9.86301 11.1682 11.9589 8.46818 15.1644 8.46818H16.8904ZM36 8.46818V0H33.6575C24.7808 0 19.2329 5.64545 19.2329 16.0773V27H28.9726V16.4455C28.9726 11.1682 31.0685 8.46818 34.274 8.46818H36Z" fill="black"></path></svg>
                         <p className='testimonial'>{ele.testimonial}</p>
@@ -39,9 +77,7 @@ const testimonials = () => {
                     </div>
                 })}
 
-            </div>
-
-            <TestimonialCarousel />
+        </Slider>
         </>
     )
 }
@@ -49,4 +85,4 @@ const testimonials = () => {
 
 
 
-export default testimonials
+export default TestimonialCarousel
